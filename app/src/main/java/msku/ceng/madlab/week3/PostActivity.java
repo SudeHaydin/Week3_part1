850804456b993b7dd2561b810eca1622804f6b19
U210709072 <sude_haydin@hotmail.com>
2025-10-10 15:48:04 +0300

    updated code

--- app/src/main/java/msku/ceng/madlab/week3/PostActivity.java
+++ app/src/main/java/msku/ceng/madlab/week3/PostActivity.java
@@ -1,14 +1,33 @@
 package msku.ceng.madlab.week3;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class PostActivity extends AppCompatActivity {
+    static final int CAPTURE_IMAGE = 0;
+    ImageView img;
+    EditText txtMessage;
+
+    ImageButton btnOk;
+
+    ImageButton btnCancel;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +39,51 @@ public class PostActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        txtMessage = findViewById(R.id.txtMessage);
+        img = findViewById(R.id.imgView);
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAPTURE_IMAGE);
+            }
+        });
+
+        btnOk = findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                Bundle bundle = new Bundle();
+                bundle.putCharSequence("msg", txtMessage.getText());
+                bundle.putParcelable("bitmap" , ((BitmapDrawable)img.getDrawable()).getBitmap());
+                intent.putExtras(bundle);
+                setResult(PostActivity.RESULT_OK);
+                finish();
+            }
+        });
+        btnCancel = findViewById(R.id.btnCancel);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(PostActivity.RESULT_CANCELED);
+                finish();
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CAPTURE_IMAGE && resultCode == PostActivity.RESULT_OK){
+            Bundle bundle = data.getExtras();
+            Bitmap image = (Bitmap) bundle.get("data");
+            img.setImageBitmap(image);
+
+        }
     }
 }
